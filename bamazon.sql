2322d1b8351db8537ef169cc74be3417b7eaad61
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-03 12:13:09 -0700

    Added a INSERT TO and VALUES method. Created 10 different Values for 10 different items in bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -14,3 +14,33 @@ CREATE TABLE products(
     PRIMARY KEY(id)
 
 );
+
+INSERT INTO products(item_name, department_name, price, stock_quantity)
+Values ("Maple Staff", "Weapon", 35000, 50)
+
+INSERT INTO products (item_name, department_name, price, stock_quantity)
+VALUES("Maple Claw", "Weapon", 70000, 50);
+
+INSERT INTO products (item_name, department_name, price, stock_quantity)
+VALUES("Maple Skanda", "Weapon", 35000, 50);
+
+INSERT INTO products (item_name, department_name, price, stock_quantity)
+VALUES("Flaming Katana", "Weapon", 10000, 50);
+
+INSERT INTO products (item_name, department_name, price, stock_quantity)
+VALUES("Maple Karstan", "Weapon", 240000, 50);
+
+INSERT INTO products (item_name, department_name, price, stock_quantity)
+VALUES("Icarus Cape", "Cape", 40000, 50);
+
+INSERT INTO products (item_name, department_name, price, stock_quantity)
+VALUES("Blue Sauna Robe", "Overall", 15000, 50);
+
+INSERT INTO products (item_name, department_name, price, stock_quantity)
+VALUES("Zakum Helmet", "Helmet", 500000, 50);
+
+INSERT INTO products (item_name, department_name, price, stock_quantity)
+VALUES("Esther Shield", "Shield", 65000, 50);
+
+INSERT INTO products (item_name, department_name, price, stock_quantity)
+VALUES("Squishy Shoes", "Shoes", 65000, 50);
