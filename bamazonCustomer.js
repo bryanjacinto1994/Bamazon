78336996ced358adf42a547c107b3286464db4b5
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-10-03 15:09:26 -0700

    Created a mysql.createConnection and included all the host, port, user, pass, and database information

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,19 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 var cliTable = require("cli-table");
+
+var connection = mysql.createConnection({
+
+    host: 'localhost',
+
+    port: 3306,
+
+    user: 'root',
+
+    password: 'password',
+
+    database: 'bamazon_db'
+
+
+});
+
